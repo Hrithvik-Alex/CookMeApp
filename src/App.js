478e1f286b6a50bd59fdc29d5ac7b961d7698cc6
Hansson <HanssonLin23@gmail.com>
2019-09-14 13:47:56 -0400

    reset again

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MainPage from './MainPage';
+
+import "tabler-react/dist/Tabler.css";
+
+function App() {
+  return (
+    <MainPage></MainPage>
+  );
+}
+
+export default App;
--- src/SiteWrapper.js
+++ src/SiteWrapper.js
@@ -1,14 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 
 import "./SiteWrapper.css";
 
 class SiteWrapper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
+  
   render() {
     return (
       <div class="container">  
