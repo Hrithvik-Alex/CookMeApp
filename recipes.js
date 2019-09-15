acdbe54ed796c8efe9adf3042877716c8c160abc
Eddy Yao <eddy2yao2@gmail.com>
2019-09-14 20:10:06 -0400

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,44 @@
+const axios = require('axios').default;
+
+const BIG_OVEN_KEY = "Rc3kEhQ293h59oU9Z853fw48CmI1H1Js"
+
+const BIG_URL = "http://api2.bigoven.com/recipes?pg=1&rpp=25&api_key=Rc3kEhQ293h59oU9Z853fw48CmI1H1Js&isbookmark=0&title_kw="
+
+const GET_ID = "http://api2.bigoven.com/recipe/"
+
+const FIREBASE_URL = "https://cookme-d0c7d.firebaseio.com/"
+
+async function get_recipe(search_string) {
+  var recipe_id
+
+  await axios.get(BIG_URL + search_string)
+  .then(function (response) {
+   recipe_id = response.data.Results[0].RecipeID
+  }).catch(function(err) {
+   console.log(err)
+  })
+
+  await axios.patch(FIREBASE_URL + "historic_search.json", {search_string: recipe_id})
+  .then(function (response) {
+  }).catch(function(err) {
+    console.log(err)
+  })
+
+  await axios.get(GET_ID + recipe_id + "?api_key=" + BIG_OVEN_KEY)
+  .then(function (response){
+    console.log(response.data)
+  }).catch(function(err){
+    console.log(err)
+  })
+}
+
+async function get_historic(){
+  await axios.get(FIREBASE_URL + "historic_search.json")
+  .then(function(response){
+    console.log(response.data)
+  }).catch(function(err){
+    console.log(err)
+  })
+}
+
+get_recipe("Lasagna")
